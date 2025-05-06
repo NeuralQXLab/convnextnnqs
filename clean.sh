46e3cbf98effab7ff8016c812a2acb2427130f73
Filippo Vicentini <filippovicentini@gmail.com>
2025-05-06 10:06:45 +0200

    remove some files

--- clean.sh
+++
@@ -1,4 +0,0 @@
-rm -rf .venv
-rm main.py
-rm pyproject.toml
-rm uv.lock
--- install.sh
+++
@@ -1,7 +0,0 @@
-uv init --python 3.12
-uv run main.py
-uv add --editable ./deepnets
-uv add matplotlib
-uv add seaborn
-uv add ipykernel
-uv sync
